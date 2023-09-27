7f8b91276d91cc95b58fc9654aa33d37d4e63449
Sarah Baker <sarahbaker@Sarahs-MacBook-Pro-2.local>
2023-09-27 14:52:35 -0500

    slight revisions, look at indexing

--- group-deliverable-1.py
+++ group-deliverable-1.py
@@ -1,37 +1,46 @@
 import csv
 
+data = []
+
 def display_list(list):
     print(list)
 
 def load_data():
-    csvfile = open('31118270.csv')
-    reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    csvfile = open('birthcontroldata.csv')
+    reader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in reader:
-        print(', '.join(row))
+        #print(', '.join(row))
+        data.append(row)
+    #print(data)
 
 def look_up_use_of_birth_control_by_education_level(educationLevel):
-    #This and look_up_use_of_birth_control_by_religion should be combined because
-    #they are repetitive...how can we do this and know which column to access?
-    user_ids = get_user_ids_by_column("educ", educationLevel)
-    get_use_of_birth_control(user_ids)
+    user_ids = get_user_ids_by_column(educationLevel)
+    #get_use_of_birth_control(user_ids)
 
 def look_up_use_of_birth_control_by_religion(religion):
     user_ids = get_user_ids_by_column("religion", religion)
-    get_use_of_birth_control(user_ids)
+    #get_use_of_birth_control(user_ids)
 
-def get_user_ids_by_column(col, topic):
-    #How to access data by column and add it to a list?
+def get_user_ids_by_column(topic):
+    educationData = []
+    for row in data:
+        print(data[row, 21])
+        #if (row, 21) == topic:
+            #educationData.append((row, 0))
+    #print(educationData)
+            
 
-def get_use_of_birth_control(user_ids):
+#def get_use_of_birth_control(user_ids):
     #This is doing more than one thing. When/how/where do we call display_list?
-    birt3_answers = []
-    for user in user_ids:
+    #birt3_answers = []
+    #for user in user_ids:
         #access birt3 column and append item to list
-        birt3_answers.append()
-    display_list(birt3_answers)
+        #birt3_answers.append()
+    #display_list(birt3_answers)
 
 def main():
     load_data()
+    look_up_use_of_birth_control_by_education_level('Four year college or university degree/Bachelor.s degree (e.g., BS, BA, AB)')
 
 if __name__ == "__main__":
     main()
