83302c82ac5d32fd73e804d3e520d6df06e76573
谢欣洁 <evelyn@xiexinjiedeMacBook-Pro.local>
2023-09-28 17:09:12 -0500

    roughly added the command line args

--- birth_control.py
+++ birth_control.py
@@ -1,56 +1,74 @@
-
 import csv
 import argparse
-def load_participants_from_csv(csv_file):
-    participants = []
-    with open(csv_file, mode='r', newline='', encoding='utf-8-sig') as file:
-        reader = csv.DictReader(file, delimiter=',')
-        for row in reader:
-            participants.append(row)
-    return participants
-def search_participants(participants, field_name, target_value, result_field):
-    matching_results = []
-    for participant in participants:
-        if participant[field_name] == target_value:
-            matching_results.append(participant[result_field])
-    return matching_results
+
+data = []
+
+def display_list(list):
+    print(list)
+
+#done
+def load_data():
+    csvfile = open('birthcontroldata.csv')
+    reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    for row in reader:
+        #print(', '.join(row))
+        data.append(row)
+    #print(data)
+
+#i think this is where we should be displaying the data
+#is this where we are passing in the input on the command line?
+def look_up_use_of_birth_control_by_education_level(educationLevel):
+    user_ids=get_user_ids_by_column_for_educ(educationLevel)
+    get_use_of_birth_control(user_ids)
+
+def look_up_use_of_birth_control_by_religion(religion):
+    user_ids = get_user_ids_by_column_for_religion(religion)
+    get_use_of_birth_control(user_ids)
+
+#done 
+def get_user_ids_by_column_for_educ(topic):
+    user_ids = []
+    for row in data:
+        if (row[21]) == topic:
+          user_ids.append(row[0])
+    #print(user_ids)
+    return user_ids
+    #get_use_of_birth_control(user_ids)
+
+#done 
+def get_user_ids_by_column_for_religion(topic):
+    user_ids = []
+    for row in data:
+        if (row[32]) == topic:
+          user_ids.append(row[0])
+    #print(user_ids)
+    return user_ids
+    #get_use_of_birth_control(user_ids)
+
+def get_use_of_birth_control(user_ids):
+    #This is doing more than one thing. When/how/where do we call display_list?
+    birt3_answers = []
+    for user in user_ids:
+        for row in data:
+        #access birt3 column and append item to list
+            if (row[0]==user):
+                birt3_answers.append(row[45])
+    #display_list(birt3_answers)
+    print(birt3_answers)
+
 def main():
+    load_data()
     parser = argparse.ArgumentParser(description="Search for participants and filter by state, religion, or political view")
     parser.add_argument("--educ", help="Educational level of participants to search for")
     parser.add_argument("--religion", help="Religion of participants to search for")
-    parser.add_argument("--state", help="State abbreviation to filter by")
-    parser.add_argument("--political", help="Political view of participants to search for")
+
     args = parser.parse_args()
-    csv_file = "birthcontrol.csv"
-    participants = load_participants_from_csv(csv_file)
-    search_field = None
-    search_value = None
-    result_field = None
-    if args.educ:
-        search_field = "educ"
-        search_value = args.educ
-        result_field = "birt3"
-    elif args.religion:
-        search_field = "religion"
-        search_value = args.religion
-        result_field = "birt3"
-    elif args.state:
-        search_field = "state"
-        search_value = args.state
-        result_field = "birt6a"
-    elif args.political:
-        search_field = "polview"
-        search_value = args.political
-        result_field = "birt7"
-    if search_field and search_value:
-        matching_results = search_participants(participants, search_field, search_value, result_field)
-        if matching_results:
-            print(f"Results for {search_field} '{search_value}':")
-            for result in matching_results:
-                print(result)
-        else:
-            print(f"No information found for {search_field} '{search_value}'")
-    else:
-        print("You must provide one of the valid command line arguments.")
+    if args.religion:
+        look_up_use_of_birth_control_by_religion(args.religion)
+    elif args.educ:
+        look_up_use_of_birth_control_by_education_level(args.educ)
+
+
+
 if __name__ == "__main__":
     main()
