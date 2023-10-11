0c9ba9dc2936c564bb42b3c6ef885f2272e0f63d
Sarah Baker <sarahbaker@Sarahs-MacBook-Pro-2.local>
2023-10-10 21:07:05 -0500

    fixing merge

--- Tests/test_birth_control.py
+++ Tests/test_birth_control.py
@@ -2,7 +2,7 @@ import unittest
 import subprocess
 from ProductionCode.birth_control import *
 
-# clean these up with the new production code
+# clean these up with the new production code, git pull for divide by zero error
 
 class TestBirthControl(unittest.TestCase):
 
@@ -10,68 +10,64 @@ class TestBirthControl(unittest.TestCase):
         load_data()
 
     def test_load_data(self):
-        """ensures load data function does not return any errors"""
+        """
+        Ensures the load data function does not return any errors.
+        """
         self.assertIsNone(load_data())
 
     def test_display_results(self):
-        """Asserts that the list passed into the display_list function
-        is indeed displayed, and that the displayed list is correct for the input provided"""
-        myList = ["Katherine", "Ella", "Evelyn"]
-        self.assertEqual(display_results(myList), myList, "Should be " + str(myList))
+        """
+        Asserts that the list passed into the display_list function
+        is indeed displayed, and that the displayed list is correct for the input provided.
+        """
+        listOfNamesToDisplay = ["Katherine", "Ella", "Evelyn"]
+        self.assertEqual(display_results(listOfNamesToDisplay), listOfNamesToDisplay, "Should be " + str(listOfNamesToDisplay))
 
-    def test_look_up_use_of_birth_control_by_religion(self):
-        """Asserts that the use of birth control by religion is being looked up
-        and not returning any errors"""
+    def test_look_up_use_of_birth_control_by_demographic(self):
+        """
+        Asserts that the use of birth control by demographic is being looked up
+        and not returning any errors.
+        """
         message = "Test value is not none."
         self.assertIsNone(look_up_use_of_birth_control_by_demographic("Hindu"), message)
 
-    def test_look_up_use_of_birth_control_by_religion_EDGECASE(self):
-        """Tests the edge case of the function receiving input
-        that is not valid, i.e. misspelled or not in database"""
+    def test_look_up_use_of_birth_control_by_demographic_EDGECASE(self):
+        """
+        Tests the edge case of the function receiving input
+        that is not valid, i.e. misspelled or not in database.
+        """
         invalidReligion = "Flying Spaghetti Monster"
-        self.assertRaises(IndexError, look_up_use_of_birth_control_by_demographic, invalidReligion)
+        self.assertEqual(look_up_use_of_birth_control_by_demographic(invalidReligion), "Sorry, this demographic is not in the dataset.", "Should be: Sorry, this demographic is not in the dataset.")
 
-    def test_get_user_ids_by_column_for_religion(self):
-        """Affirms that the user ids for the correct religion are returned"""
+    def test_get_user_ids_by_column(self):
+        """
+        Affirms that the user ids for the correct demographic are returned
+        """
         ids = ['50000198', '50000290', '70000589', '70000664', '70000805']
         self.assertEqual(get_user_ids_by_column("Hindu"), ids, "Should be " + str(ids))
 
-    def test_get_user_ids_by_column_for_religion_EDGECASE(self):
-        """Affirms no user ids are returned if religion input is invalid
-        and that IndexError is thrown"""
+    def test_get_user_ids_by_column_EDGECASE(self):
+        """
+        Affirms no user ids are returned if the demogrpahic input is invalid
+        and that the error message is printed.
+        """
         invalidReligion = "Flying Spaghetti Monster"
-        self.assertRaises(IndexError, get_user_ids_by_column, invalidReligion)
+        self.assertEqual(get_user_ids_by_column(invalidReligion), "Sorry, this demographic is not in the dataset.", "Should be: Sorry, this demographic is not in the dataset.")
 
     def test_get_use_of_birth_control(self):
-        """Affirms the correct list of birth control usage is displayed"""
+        """
+        Affirms the correct list of birth control usage is displayed based on the demographic.
+        """
         ids = ['50000198', '50000290',  '70000589', '70000664', '70000805']
         output = ['Every time', 'Never', 'Every time', 'Not applicable/Does not have vaginal intercourse/sex', 'Never']
         self.assertEqual(get_use_of_birth_control(ids), output, "Should be " + str(output))
 
     def test_get_use_of_birth_control_EDGECASE(self):
-        """Affirms that an error is raised if the function
-        is passed an empty list as input"""
-        self.assertRaises(IndexError, get_use_of_birth_control, [])
-
-    def test_look_up_use_of_birth_control_by_education_level(self):
-        """ensure no errors are returned"""
-        self.assertIsNone(look_up_use_of_birth_control_by_demographic("Less than high school (Grades 1-8 or no formal schooling)"))
-
-    def test_edge_look_up_use_of_birth_control_by_education_level(self):
-        """edge case for if topic entered into lookup function is not included in the dataset"""
-        self.assertRaises(IndexError, look_up_use_of_birth_control_by_demographic, "Nonexistent education level")
-
-    def test_get_user_ids_by_column_for_educ(self):
-        """example test for education level"""
-        ids= ['50000211','50000290','50000354','50000355','50000385','50000390','70000080','70000113','70000136','70000155','70000292',
-        '70000315','70000325','70000350','70000498','70000515','70000548','70000590','70000682','70000762','70000794','70000859']
-        #load_data()
-        outcome= get_user_ids_by_column("Less than high school (Grades 1-8 or no formal schooling)")
-        self.assertEqual(outcome, ids, "Should be " + str(ids))
-    
-    def test_edge_get_user_ids_by_column_for_educ(self):
-        """tests edge case that if education level is not included in the dataset returns error for function called by lookup function"""
-        self.assertRaises(IndexError, get_user_ids_by_column, "Nonexistent education level")
+        """
+        Affirms that a message is printed if the function
+        is passed an empty list as input.
+        """
+        self.assertEqual(get_use_of_birth_control([]), "List is empty", "Should be: List is empty")
 
     def test_main(self):
         """check if birth_control.py works for valid command line argument"""
