52c99ed193d37675c23e40930dfb460fbeac1bc9
谢欣洁 <evelyn@xiexinjiedeMacBook-Pro.local>
2023-10-27 09:16:23 -0500

    GD4Data

---
+++ Data/createtable.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS test;
+CREATE TABLE test (
+  col1 text,
+  col2 text,
+  col3 text,
+  col4 text,
+  col5 text,
+  col6 text,
+  col7 text,
+  col8 text,
+  col9 text,
+  col10 text,
+  col11 text,
+  col12 text,
+  col13 text,
+  col14 text
+);
